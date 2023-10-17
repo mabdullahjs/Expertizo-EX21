dc2cd1cdbf1f3ab9443d668c8d34cf73ea2eb37c
Muhammad Abdullah <101110601+mabdullahjs@users.noreply.github.com>
2023-10-17 09:24:03 +0500

    alert, prompt, variables and datatypes

---
+++ javascript/01- Introduction/app.js
@@ -0,0 +1,120 @@
+// console.log("hello world");
+// console.log("4 + 4");
+
+
+//data types
+
+//string "commas ma jo bhi likhonga as it is"
+//number 54.54545
+//boolean true || false
+
+
+
+
+
+
+// console.log(4 / 4);
+// console.log(4 + "4");
+// console.log(4 + "4");
+
+
+
+// alert('hello world');
+// console.log('hello world');
+// console.log('hello world');
+// console.log('hello world');
+// console.log('hello world');
+// console.log('hello world');
+// console.log('hello world');
+// console.log('hello world');
+// console.log('hello world');
+// console.log('hello world');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var names = 'Abdullah';
+// let names = 'abdullah';
+// const names = 'abdullah';
+// console.log(names);
+
+
+
+
+// var
+// reassign == true
+// redeclare == true 
+
+
+
+// let
+// reassign == true
+// redeclare == false
+
+
+
+// const
+// reassign == false
+// redeclare == false
+
+
+// var names = 'abdullah';
+// names = 'usman';
+// console.log(names);
+
+// let names = 'abdullah';
+// // let names = 'abdullah'
+// names = 'usman';
+// console.log(names);
+
+
+// const names = 'abdullah'
+// names = 'usman'
+// console.log(names);
+
+
+
+
+
+
+// var names = 'abdullah';
+// console.log(names);
+
+
+
+
+
+let userName = prompt('what is your name');
+console.log(userName);
+
+
+
+
+
+
+
+
+
+
+
+
+
