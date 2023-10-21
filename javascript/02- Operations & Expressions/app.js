ac1efe785a88589980a1c783505e968b20bad93f
mabdullahjs <mabdullah2037@outlook.com>
2023-10-21 20:31:52 +0500

    conditions

---
+++ javascript/02- Operations & Expressions/app.js
@@ -0,0 +1,202 @@
+// let names = 'abdullah';
+// let names = 'abd'
+
+// var names = 'abdullah';
+// var names = 'abd';
+
+
+// let names = 'abdullah';
+// console.log(names);
+// names = 'ammar'
+// console.log(names);
+
+
+
+//string "23" || '43'
+//number 54 || 54.656554
+//boolean true || false
+
+//undefined
+//object
+
+
+
+
+
+
+// let user = Number('34');
+// console.log(user);
+//greetUser
+
+
+
+// let user = String(34);
+// console.log(typeof(user));
+// console.log(user);
+
+
+// let user = Boolean('');
+// console.log(user);
+
+
+
+
+
+
+// let names = 'abdullah';
+// let age = 20
+// console.log("My name is " + names + "\n and my age is " + age + ".");
+
+
+
+
+
+//Arithmatic Operator
+
+// +
+// -
+// *
+// /
+// %
+
+//Assignment Operator
+// =
+// +=
+// -=
+// /=
+// *=
+
+//Comparision Operator
+
+// > greater than
+// < less than
+// == equal to
+// ===  double equal to
+// != // not equal
+// >= //greater than and equal to
+// <= // less than and equal to
+
+//logical Operator
+// &&
+// ||
+// !
+
+
+
+// let num1 = 2;
+// let num2 = 2;
+// let sum = num1 % num2;
+// console.log(sum);
+
+
+
+
+
+//percentage
+
+// let userName = prompt('Enter your name');
+// let chemMarks = Number(prompt('Enter chemistry marks'));
+// let phyMarks = Number(prompt('Enter physics marks'));
+// let mathsMarks = Number(prompt('Enter maths marks'));
+// let bioMarks = Number(prompt('Enter biology marks'));
+
+// let totalMarks = 400;
+// let userTotal = chemMarks + phyMarks + mathsMarks + bioMarks
+// let percentage = userTotal / totalMarks * 100;
+
+// console.log('student name ' + userName + " got " + percentage + "%.");
+
+
+
+
+
+
+// let num = 1;
+// num = num + 2;
+// num += 2
+// console.log(num);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let num1 = 10;
+// let num2 = 10;
+// let cond = num1 > num2;
+// let cond = num1 < num2;
+// let cond = num1 === num2;
+// let cond = num1 === num2;
+// let cond = num1 >= num2;
+// let cond = num1 <= num2;
+// let cond = num1 == num2;
+
+// console.log(cond)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// &&
+// ||
+// let condition = 10 > 9 || 9 < 6;
+// let condition2 = 10 > 9 && 9 < 6;
+
+
+
+
+
+
+
+
+
+
+
+
+
+;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
