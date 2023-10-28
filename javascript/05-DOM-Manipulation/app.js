5305be9d49d8720abe0c172b3a1c2cfc09ffe43e
mabdullahjs <mabdullah2037@outlook.com>
2023-10-28 20:37:17 +0500

    dom manipulation done

---
+++ javascript/05-DOM-Manipulation/app.js
@@ -0,0 +1,136 @@
+// let 
+// var
+// const
+
+// ==
+// ===
+
+
+// let a = 3 !== "3"
+// console.log(a);
+
+// ==
+// ===
+// !=
+// !==
+
+
+
+
+
+
+
+
+
+
+
+
+// if(10 < 5 || 10 > 2 && 10 < 5 && 10 < 19){
+//     console.log('if chala ga');
+// }else{
+//     console.log('else chala ga');
+// }
+
+
+
+
+
+
+// let name = 'abdullah'
+// function greetUser(name){
+//     console.log('hello ' + name);
+//     return  
+// }
+
+// console.log(greetUser());
+
+
+
+
+
+
+
+
+
+
+
+
+
+const h1 = document.querySelector('h1');
+// h1.innerHTML = 'changed through <br/> javascript'
+
+function greetUser (){
+    if(h1.innerHTML === 'changed through javascript'){
+        h1.innerHTML = 'hello world'
+    }else{
+        h1.innerHTML = 'changed through javascript'
+    }
+    // console.log('hello world');
+}
+
+
+
+
+
+
+
+
+
+
+// create two prompts  p1 and p2 and manipulatate dom like this p1 + p2 value = p1+p2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
