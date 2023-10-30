dd021757bcfd4359c9bbf42d7d50e3419fc1e1dc
mabdullahjs <mabdullah2037@outlook.com>
2023-10-30 20:31:21 +0500

    array

---
+++ javascript/06-Array/app.js
@@ -0,0 +1,151 @@
+// const head = document.querySelector('.head');
+
+// const num1 = +prompt('enter first number')
+// const num2 = +prompt('enter second number')
+
+// function changeHead(){
+//     return head.innerHTML = num1 + num2
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let fruit = ['mango' , 'orange' , 20 , 'banana' , true]
+// console.log(fruit[2]);
+
+
+//push
+//pop
+//unshift
+//shift
+//splice
+//slice
+//split
+//join
+//concat
+
+
+//sort
+//reverse
+//indexof
+
+
+
+
+
+
+let fruit = ['mango', 'orange', 'guava', 'banana', 'grapes']
+
+let num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// fruit.push('watermelon') // add item at the end
+
+// fruit.pop() // delete item from end
+
+// fruit.shift(); // delete item from start
+
+// fruit.unshift('watermelon'); // add item at start
+
+// fruit.splice(0 , 2 , 'apple'); // remove add item in an array
+
+// const newArr = fruit.slice(1 , 4) // create new array
+
+// const newArr = num.slice(2 , 7)
+
+const names = 'Muhammad Abdullah khan';
+
+const createArr = names.split(' ');
+
+const fruitString = fruit.join()
+
+console.log(createArr);
+
+
+// console.log(newArr);
+
+//all fruits names are mango, orange, guava, banana, grapes
+
+
+
+
+
+
+
+
+
+
+
+
+// num.splice(0 , 1);
+// // console.log(num);
+// num.splice(3 , 1);
+// // console.log(num);
+// num.splice(7 , 1);
+// console.log(num);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
