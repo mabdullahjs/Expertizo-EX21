6c74b6ae209a586d30b6c3cebc001a236d439599
mabdullahjs <mabdullah2037@outlook.com>
2023-11-06 20:32:30 +0500

    todo app completed

--- javascript/08-Todo-App/app.js
+++ javascript/08-Todo-App/app.js
@@ -1,9 +1,47 @@
-const todoForm = document.querySelector('#todo');
+const todo = document.querySelector('#todo');
 const ul = document.querySelector('#ul');
 
 
-function addTodo(){
-    console.log(todoForm.value);
-    ul.innerHTML += `<li>abcd </li>`;
+const arr = [];
+function renderTodo() {
+    for (let i = 0; i < arr.length; i++) {
+        ul.innerHTML += `
+        <li>${arr[i]}
+        <button onclick="deleteTodo(${i})">delete</button>
+        <button onclick="editTodo(${i})">edit</button>
+        </li>`
+    }
 }
+function addTodo() {
+    ul.innerHTML = ''
+    arr.push(todo.value);
+    console.log(arr);
+    renderTodo()
+    todo.value = '';
+}
+
+
+function deleteTodo(i) {
+    ul.innerHTML = ''
+    console.log('todo deleted', arr[i]);
+    arr.splice(i, 1);
+    console.log(arr);
+    renderTodo()
+
+}
+function editTodo(i) {
+    ul.innerHTML = ''
+    // console.log('todo edited' , arr[i]);
+    const editedVal = prompt('enter value to update');
+    arr.splice(i, 1, editedVal);
+    renderTodo()
+}
+
+
+// const fruit = ['apple' , 'banana' , 'orange'];
+// for (let i = 0; i < fruit.length; i++) {
+//     console.log(fruit[i]);
+// }
+
+// renderTodo()
 
