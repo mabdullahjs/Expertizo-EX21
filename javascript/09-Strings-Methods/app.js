b78c015e987154216e64a62970eee542c39710ca
mabdullahjs <mabdullah2037@outlook.com>
2023-11-08 20:33:38 +0500

    string methods

---
+++ javascript/09-Strings-Methods/app.js
@@ -0,0 +1,313 @@
+// if("karachi" === "Karachi"){
+//     console.log('karachi is the safest city');
+// }else{
+//     console.log('Mobile nikaal');
+// }
+
+
+
+// let Names;
+// let names
+
+
+
+
+
+
+
+
+
+
+// let names = 'ABdullah';
+// // console.log(names.toLowerCase());
+// console.log(names.toUpperCase());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const city = prompt('Enter city name');
+// if(city.toLowerCase() === "karachi"){
+//     console.log('karachi is the safest city');
+// }else{
+//     console.log('Mobile nikaal');
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const city = prompt('Enter city name');
+// const cities = ['karachi' , 'hyderabad' , 'london' , 'california' , 'shikarpur' , 'thatta'];
+
+// let isAvailable = false
+// for (let i = 0; i < cities.length; i++) {
+//     if(city.toLowerCase() === cities[i]){
+//         console.log('city available');
+//         isAvailable = true
+//         break
+//     }
+// }
+// if(isAvailable === false){
+//     console.log('tum is pritvi ka nahi');
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let city = 'karaChi!';
+// console.log(city.toLowerCase());
+// console.log(city.toUpperCase());
+// console.log(city.slice(0 , 3));
+// console.log(city.length);
+// const newVar = city.trim()
+// console.log(newVar.length);
+// console.log(city.charAt(city.length-1));
+
+// for (let i = 0; i < city.length; i++) {
+//     if (city.charAt(i) === "!") {
+//         alert("Exclamation point found!");
+//         break;
+//     }
+// }
+
+
+
+
+
+
+
+// const str = 'In previous chapters you learned previous two different ways to replace';
+
+// console.log(str.replaceAll('previous' , 'next'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let myName = 'the variable doesnt have the value';
+// let nameArr = myName.split(' ');
+
+
+
+// let string = ''
+// let userInput = +prompt('enter number')
+// for (let i = 0; i <= userInput; i++) {
+//     string = string + '*';
+//     if (i === userInput) {
+//         console.log(string.length-1);
+//         for (let j = 0; j < string.length; j++) {
+//             console.log(string.slice(0 , string.length-j));
+//         }
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let result = [];
+// for (let i = 0; i < nameArr.length; i++) {
+//     console.log(nameArr[i].slice(0 , 1));
+//     let firstLetter = nameArr[i].slice(0 , 1).toUpperCase();
+//     let lastLetter = nameArr[i].slice(1).toLowerCase();
+//     let completeLetter = firstLetter + lastLetter;
+//     result.push(completeLetter);
+// }
+// console.log(result.join(' '));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
