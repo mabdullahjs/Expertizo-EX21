32b9e6461ee7789d5298791e7d822bf1917a2665
mabdullahjs <mabdullah2037@outlook.com>
2023-11-13 20:17:54 +0500

    date intro

---
+++ javascript/11-Switchcase/app.js
@@ -0,0 +1,81 @@
+// let grade = 'A';
+
+
+// if(grade === 'A'){
+//     console.log('A grade');
+// }else if(grade === 'B'){
+//     console.log('B grade');
+// }else if(grade === 'C'){
+//     console.log('C grade');
+// }else{
+//     console.log('warr gaya');
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let grade = 'Br';
+
+// switch (grade) {
+//     case 'A':
+//         console.log('A grade');
+//         break
+//     case 'B':
+//         console.log('B grade');
+//         break
+//     case 'C':
+//         console.log('C grade');
+//     default:
+//         console.log('program Warr gaya');
+// }
+
+
+
+
+
+
+
+
+let num = 10;
+// if(num > 5){
+//     console.log('if chal rha ha');
+// }else{
+//     console.log('else chal rha ha');
+// }
+
+
+
+
+// switch(num){
+//     case num > 5:
+//         console.log('case chal rha ha');
+//         if(num > 2){
+//             console.log('if chal rha ha');
+//         }else{
+//             console.log('else chal rha ha');
+//         }
+//         break
+//     default:
+//         console.log('default chal rha ha');
+// }
+
+
+
+
---
+++ javascript/12-Date/app.js
@@ -0,0 +1,73 @@
+// const text = document.querySelector('#text');
+
+
+
+
+// let arr = []
+// function greetUser(){
+//     arr.push(text.value);
+
+//     console.log(arr);
+//     text.value = ''
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let currentDate = new Date();
+// console.log(typeof(currentDate));
+// console.log(currentDate.toLocaleString());
+// console.log(currentDate.getDate());
+// console.log(currentDate.getDay());
+console.log(currentDate.getMonth());
+// console.log(currentDate);
+// console.log(currentDate);
+// console.log(currentDate);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
