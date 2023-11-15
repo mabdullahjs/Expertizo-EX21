b069f52d3951eac3065e85f3969d2d1338acf5ec
mabdullahjs <mabdullah2037@outlook.com>
2023-11-15 20:35:09 +0500

    date and time completed

--- javascript/12-Date/app.js
+++ javascript/12-Date/app.js
@@ -46,15 +46,82 @@
 
 
 
-let currentDate = new Date();
+// let currentDate = new Date();
 // console.log(typeof(currentDate));
 // console.log(currentDate.toLocaleString());
 // console.log(currentDate.getDate());
 // console.log(currentDate.getDay());
-console.log(currentDate.getMonth());
-// console.log(currentDate);
-// console.log(currentDate);
-// console.log(currentDate);
+// console.log(currentDate.getMonth());
+// console.log(currentDate.getFullYear());
+// console.log(currentDate.getHours());
+// console.log(currentDate.getTime());
+
+
+
+
+
+
+
+
+// let currentDate = new Date('November 27,2003');
+// let days = ['sun', 'mon', 'tues', 'wed', 'thur', 'fri', 'sat'];
+// let index = currentDate.getDay()
+// console.log(days[index]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let currentDate = new Date();
+// let harisBirthDate = new Date('May 13, 1982');
+
+// let currentTime = currentDate.getTime();
+// let harisTime = harisBirthDate.getTime();
+
+// let remaining = currentTime - harisTime;
+// console.log('Miliseconds===>', remaining);
+// console.log('seconds===>', Math.ceil(remaining / 1000));
+// console.log('Minutes===>', Math.ceil(remaining / 1000 / 60));
+// console.log('hours===>', Math.ceil(remaining / 1000 / 60 / 60));
+// console.log('days===>', Math.ceil(remaining / 1000 / 60 / 60 / 24));
+// console.log('weeks===>', Math.ceil(remaining / 1000 / 60 / 60 / 24 / 7));
+// console.log('months===>', Math.ceil(remaining / 1000 / 60 / 60 / 24 / 30));
+// console.log('years===>', Math.ceil(remaining / 1000/ 60 / 60 / 24 / 30 /12));
+
+
+
+
 
 
 
