5df2d03bb9c6e5c641d034dfc197a4f2f27ef060
mabdullahjs <mabdullah2037@outlook.com>
2023-11-18 19:10:29 +0500

    obj

---
+++ javascript/13-Objects/app.js
@@ -0,0 +1,123 @@
+// console.log('hello objects');
+
+
+
+// const arr = [];
+// arr.push('abd');
+// console.log(arr.push);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const arr = ['1' , 2 , 3 , 4];
+// console.log(arr[2]);
+
+
+
+// const user = {
+//     name: 'abdullah',
+//     email: 'mabdullah2037@gmail.com',
+//     age: 20,
+//     isLoggedIn: false,
+// }
+// console.log(user.age);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const user = {
+    name:'abdullah',
+    nestedObj: {
+        nestedArr:['abdullah' , {
+            name:'abd2',
+            nestedObj:{
+                name:'abd3'
+            }
+        }]
+    }
+}
+console.log(user.nestedObj.nestedArr[1].nestedObj.name);
+
+
+
+
+
+
+
+
+
+
+
+
+
