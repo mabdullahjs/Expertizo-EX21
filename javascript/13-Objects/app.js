4267a7116daacb5b7d8644a4d1861242ba054833
mabdullahjs <mabdullah2037@outlook.com>
2023-11-18 19:10:49 +0500

    obj

--- javascript/13-Objects/app.js
+++ javascript/13-Objects/app.js
@@ -96,18 +96,18 @@
 
 
 
-const user = {
-    name:'abdullah',
-    nestedObj: {
-        nestedArr:['abdullah' , {
-            name:'abd2',
-            nestedObj:{
-                name:'abd3'
-            }
-        }]
-    }
-}
-console.log(user.nestedObj.nestedArr[1].nestedObj.name);
+// const user = {
+//     name:'abdullah',
+//     nestedObj: {
+//         nestedArr:['abdullah' , {
+//             name:'abd2',
+//             nestedObj:{
+//                 name:'abd3'
+//             }
+//         }]
+//     }
+// }
+// console.log();
 
 
 
@@ -121,3 +121,44 @@ console.log(user.nestedObj.nestedArr[1].nestedObj.name);
 
 
 
+let complexObject = {
+    person: {
+        name: {
+            first: "John",
+            last: "Doe",
+        },
+        age: 30,
+        address: {
+            street: {
+                number: 123,
+                name: "Nested Street",
+            },
+            city: "Nested City",
+            country: "Nested Country",
+        },
+    },
+    work: {
+        company: {
+            name: "TechCorp",
+            location: {
+                city: "TechCity",
+                country: "TechCountry",
+            },
+        },
+        position: "Senior Developer",
+        projects: [
+            {
+                name: "Project A",
+                technologies: ["JavaScript", "React", "Node.js"],
+            },
+            {
+                name: "Project B",
+                technologies: ["Python", "Django", "PostgreSQL"],
+            },
+        ],
+    },
+    hobbies: {
+        indoor: ["Reading", "Chess"],
+        outdoor: ["Hiking", "Cycling"],
+    },
+};
