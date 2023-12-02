0595f295c3f73dd5f827648c441a4ccff8e7fa79
mabdullahjs <mabdullah2037@outlook.com>
2023-12-02 20:30:36 +0500

    phone app

--- javascript/16-Phone-App/app.js
+++ javascript/16-Phone-App/app.js
@@ -57,16 +57,18 @@ let data;
 const div = document.querySelector('.render-phones');
 const checkout = document.querySelector('#checkout');
 
-axios.get('https://fakestoreapi.com/products')
+
+axios.get('https://api.escuelajs.co/api/v1/products')
 .then((res)=>{
-    // console.log(res.data);
+    console.log(res.data);
     data = res.data
     for (let i = 0; i < res.data.length; i++) {
+        // console.log(res.data[i].images[0]);
         // console.log(res.data[i]);  
         div.innerHTML += `
         <div style="border: 1px solid black;" class="m-5 p-5 rounded">
         <h1>title: ${res.data[i].title}</h1> <br/>
-        <img width="200px" src="${res.data[i].image}" alt="product-image">
+        <img width="200px" src="${res.data[i].images[0]}" alt="product-image">
         <h3>price: ${res.data[i].price}</h3>
         <button onclick="addToCart(${i})" class="btn btn-primary m-2 btn-lg">Add to cart</button>
         </div>
@@ -80,6 +82,11 @@ function addToCart (index){
     // console.log(data[index]);
     arr.push(data[index]);
     console.log(arr);
+    Swal.fire({
+        title: "Good job!",
+        text: "Product added to cart successfully",
+        icon: "success"
+      });
 }
 
 
--- javascript/16-Phone-App/checkout.js
+++ javascript/16-Phone-App/checkout.js
@@ -5,14 +5,28 @@ console.log(items);
 
 
 const div = document.querySelector('.render-phones');
-for (let i = 0; i < items.length; i++) {
-    // console.log(items[i]);  
-    div.innerHTML += `
-    <div style="border: 1px solid black;" class="m-5 p-5 rounded">
-    <h1>title: ${items[i].title}</h1> <br/>
-    <img width="200px" src="${items[i].image}" alt="product-image">
-    <h3>price: ${items[i].price}</h3>
-    <button onclick="addToCart(${i})" class="btn btn-primary m-2 btn-lg">Add to cart</button>
-    </div>
-    `
+
+function renderItems(){
+    if(items.length > 0){
+        div.innerHTML = ''
+    for (let i = 0; i < items.length; i++) {
+        div.innerHTML += `
+        <div style="border: 1px solid black;" class="m-5 p-5 rounded">
+        <h1>title: ${items[i].title}</h1> <br/>
+        <img width="200px" src="${items[i].image}" alt="product-image">
+        <h3>price: ${items[i].price}</h3>
+        <button onclick="deleteItem(${i})" class="btn btn-primary m-2 btn-lg">Delete</button>
+        </div>
+        `
+    }
+    }else{
+        div.innerHTML = 'no item found'
+    }
+}
+renderItems()
+function deleteItem(index){
+    items.splice(index , 1);
+    renderItems()
+    localStorage.setItem('items' , items);
+
 }
