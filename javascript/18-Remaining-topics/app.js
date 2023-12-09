0722b7f4c209527a8edef203edbb011fc1ff1f07
mabdullahjs <mabdullah2037@outlook.com>
2023-12-09 20:05:41 +0500

    remaining topics

---
+++ javascript/18-Remaining-topics/app.js
@@ -0,0 +1,228 @@
+// // // // const h2 = document.querySelector(".h2");
+// // // // const btn = document.querySelector("button");
+// // // // let seconds = 0;
+// // // // const timeout = setInterval(() => {
+// // // //   // console.log('hello world');
+// // // //   h2.innerHTML = ''
+// // // //   seconds += 1;
+// // // //   h2.innerHTML += seconds;
+// // // // }, 1000);
+
+// // // // btn.addEventListener("click", () => {
+// // // //   console.log("interval stop");
+// // // //   clearInterval(timeout);
+// // // // });
+
+// // const h2 = document.querySelector(".h2");
+// // const btn = document.querySelector("button");
+
+// // let seconds = 0
+// // const time = setInterval(() => {
+// //     if(seconds === 60){
+// //         seconds = 0
+// //     }else{
+// //         seconds += 1
+// //     }
+// //     h2.innerHTML = seconds
+// //     // console.log(seconds);
+// // }, 1000);
+
+// // btn.addEventListener('click' , ()=>{
+// //     clearInterval(time)
+// // })
+
+// // console.log(a);
+// // var a = 10;
+
+// // console.log(abc(10 , 20));
+
+// // const arr = [
+// //     {
+// //         name:'abdullah',
+// //         age:20
+// //     },
+// //     {
+// //         name:'abdullah2',
+// //         age:10
+// //     },
+// //     {
+// //         name:'abdullah3',
+// //         age:15
+// //     },
+// //     {
+// //         name:'abdullah4',
+// //         age:18
+// //     },
+// // ]
+
+// // for (let i = 0; i < arr.length; i++) {
+// //     if(arr[i].age > 15){
+// //         console.log(arr[i]);
+// //     }
+// // }
+
+// // const arr = [1, 2, 3, 4, 5, 6, 7];
+// // for (const i of arr) {
+// //     console.log(i);
+// // }
+
+// // const obj = {
+// //   name: "abdullah4",
+// //   age: 18,
+// // };
+// // console.log(obj['name']);
+
+// // for (const key in obj) {
+// //     console.log(`${key} : ${obj[key]}`);
+// // }
+
+// const arr = [1, 2, 3, 4, 5, 656];
+
+// // arr.forEach(function (item){
+// //     console.log(item);
+// // })
+
+// // const item = arr.forEach(function(item){
+// //     return item
+// // })
+// // console.log(item);
+
+// // const item = arr.map(function(item){
+// //     return item
+// // })
+// // console.log(item);
+
+// // const item = arr.map( item => item);
+// // console.log(item);
+
+// // const func = (item) => "hello world";
+
+// // console.log(func());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // const regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g
+// // const email = 'mabdullahgmail.com';
+
+// // console.log(regex.test(email));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // const regex = /^.{6,}$/
+// // const str = 'asdsadsfds';
+// // console.log(regex.test(str));
+
+
+
+
+
+
+
+
+
+
+
+// const regex = /^([0-9]{5})[\-]([0-9]{7})[\-]([0-9]{1})+/
+// const str = '42101-5334351'
+// console.log(regex.test(str));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
