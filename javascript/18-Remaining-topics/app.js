9d4e648e70f0d48aa65f3937fdc9abdbb519182b
mabdullahjs <mabdullah2037@outlook.com>
2023-12-09 20:06:42 +0500

    remaining topics

--- javascript/18-Remaining-topics/app.js
+++ javascript/18-Remaining-topics/app.js
@@ -1,228 +1,104 @@
-// // // // const h2 = document.querySelector(".h2");
-// // // // const btn = document.querySelector("button");
-// // // // let seconds = 0;
-// // // // const timeout = setInterval(() => {
-// // // //   // console.log('hello world');
-// // // //   h2.innerHTML = ''
-// // // //   seconds += 1;
-// // // //   h2.innerHTML += seconds;
-// // // // }, 1000);
+const phones = [
+    {
+        brand: 'Samsung',
+        model: 'S20',
+        ram: 8,
+        rom: 256,
+        camera: '20 megapixel',
+        price: 15000
+    },
+    {
+        brand: 'Xiomi',
+        model: 'note10',
+        ram: 4,
+        rom: 64,
+        camera: '10 megapixel',
+        price: 15000
+    },
+    {
+        brand: 'Infinix',
+        model: 'z10',
+        ram: 2,
+        rom: 16,
+        camera: '5 megapixel',
+        price: 15000
+    },
+    {
+        brand: 'Tecno',
+        model: 'spark10',
+        ram: 12,
+        rom: 512,
+        camera: '25 megapixel',
+        price: 15000
+    },
+    {
+        brand: 'Iphone',
+        model: '14',
+        ram: 4,
+        rom: 1024,
+        camera: '30 megapixel',
+        price: 15000
+    },
+    {
+        brand: 'Oppo',
+        model: 'F11',
+        ram: 8,
+        rom: 256,
+        camera: '20 megapixel',
+        price: 15000
+    },
+    {
+        brand: 'Vivo',
+        model: 'y20',
+        ram: 4,
+        rom: 64,
+        camera: '8 megapixel',
+        price: 15000
+    },
+    {
+        brand: 'Abdullah',
+        model: 's50',
+        ram: 50,
+        rom: 1024,
+        camera: '60 megapixel',
+        price: 300000
+    },
+
+]
+
+const div = document.querySelector('.container');
+const checkoutBtn = document.querySelector('#checkout-btn');
+
+for (let i = 0; i < phones.length; i++) {
+    // console.log(phones[i]);
+    div.innerHTML += `
+    <div class="p-5 border border-secondary-subtle m-5">
+    brand:${phones[i].brand}<br/>
+    Model:${phones[i].model}<br/>
+    price:${phones[i].price}<br/>
+    <button class="btn btn-primary my-2" onclick="addToCart(${i})">Add to Cart</button>
+    </div> 
+    `
+    
+}
+
+const data = JSON.parse(localStorage.getItem('cartItems'));
+let arr;
+if(Array.isArray(data)){
+    arr = [...data];
+}else{
+    arr = []
+}
+function addToCart(index){
+    console.log('add to cart called' , index);
+    arr.push(phones[index]);
+    console.log(arr);
+}
+
+
+function checkout (){
+    window.location = 'checkout.html'
+    localStorage.setItem('cartItems',JSON.stringify(arr))
+}
 
-// // // // btn.addEventListener("click", () => {
-// // // //   console.log("interval stop");
-// // // //   clearInterval(timeout);
-// // // // });
-
-// // const h2 = document.querySelector(".h2");
-// // const btn = document.querySelector("button");
-
-// // let seconds = 0
-// // const time = setInterval(() => {
-// //     if(seconds === 60){
-// //         seconds = 0
-// //     }else{
-// //         seconds += 1
-// //     }
-// //     h2.innerHTML = seconds
-// //     // console.log(seconds);
-// // }, 1000);
-
-// // btn.addEventListener('click' , ()=>{
-// //     clearInterval(time)
-// // })
-
-// // console.log(a);
-// // var a = 10;
-
-// // console.log(abc(10 , 20));
-
-// // const arr = [
-// //     {
-// //         name:'abdullah',
-// //         age:20
-// //     },
-// //     {
-// //         name:'abdullah2',
-// //         age:10
-// //     },
-// //     {
-// //         name:'abdullah3',
-// //         age:15
-// //     },
-// //     {
-// //         name:'abdullah4',
-// //         age:18
-// //     },
-// // ]
-
-// // for (let i = 0; i < arr.length; i++) {
-// //     if(arr[i].age > 15){
-// //         console.log(arr[i]);
-// //     }
-// // }
-
-// // const arr = [1, 2, 3, 4, 5, 6, 7];
-// // for (const i of arr) {
-// //     console.log(i);
-// // }
-
-// // const obj = {
-// //   name: "abdullah4",
-// //   age: 18,
-// // };
-// // console.log(obj['name']);
-
-// // for (const key in obj) {
-// //     console.log(`${key} : ${obj[key]}`);
-// // }
-
-// const arr = [1, 2, 3, 4, 5, 656];
-
-// // arr.forEach(function (item){
-// //     console.log(item);
-// // })
-
-// // const item = arr.forEach(function(item){
-// //     return item
-// // })
-// // console.log(item);
-
-// // const item = arr.map(function(item){
-// //     return item
-// // })
-// // console.log(item);
-
-// // const item = arr.map( item => item);
-// // console.log(item);
-
-// // const func = (item) => "hello world";
-
-// // console.log(func());
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // const regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g
-// // const email = 'mabdullahgmail.com';
-
-// // console.log(regex.test(email));
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // const regex = /^.{6,}$/
-// // const str = 'asdsadsfds';
-// // console.log(regex.test(str));
-
-
-
-
-
-
-
-
-
-
-
-// const regex = /^([0-9]{5})[\-]([0-9]{7})[\-]([0-9]{1})+/
-// const str = '42101-5334351'
-// console.log(regex.test(str));
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+checkoutBtn.addEventListener('click' , checkout)
